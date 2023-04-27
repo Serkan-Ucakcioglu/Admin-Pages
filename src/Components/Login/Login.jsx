e6b766d6fd45ff4f1bedacb81f77d1da5d9013bc
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-27 15:08:22 +0300

    form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import InputList from "./InputList";
 
 function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      {" "}
-      <div className="relative  flex flex-col justify-center min-h-screen overflow-hidden">
+    <div className="flex justify-center mb-4">
+      <div className="w-[500px] flex flex-col justify-center min-h-screen overflow-hidden">
         <div className="w-full p-7 m-auto bg-white rounded-md shadow border border-gray-200 lg:max-w-xl">
           <h1 className="text-3xl font-semibold text-center text-black">
             Sign in
           </h1>
 
-          <form className="mt-6">
+          <form className="mt-6" onSubmit={onSubmit}>
             <InputList />
             <div className="mt-8">
-              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-purple-600">
+              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none">
                 Login
               </button>
             </div>
