b2c1c9a062d50d48c27e7fabb142bef8e2a9de27
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-27 15:02:50 +0300

    border fix

--- src/Components/Login/InputList.jsx
+++ src/Components/Login/InputList.jsx
@@ -6,25 +6,25 @@ function InputList() {
       <div className="mb-4">
         <label
           htmlFor="email"
-          className="block text-sm font-semibold text-gray-800"
+          className="block text-left text-sm font-semibold text-gray-800"
         >
           Email
         </label>
         <input
           type="name"
-          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          className="block w-full outline-none px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md hover:border-black"
         />
       </div>
       <div className="mt-4">
         <label
           htmlFor="password"
-          className="block text-sm font-semibold text-gray-800"
+          className="block text-left  text-sm font-semibold text-gray-800"
         >
           Password
         </label>
         <input
           type="password"
-          className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          className="block w-full px-4 outline-none py-2 mt-2 text-purple-700 bg-white border rounded-md hover:border-black"
         />
       </div>
     </>
