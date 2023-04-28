35946d1c42ff0e2239c446594bbff25d5581aed1
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-28 17:20:48 +0300

    file create update

--- src/Components/Auth/RequireAuth.jsx
+++ src/Components/Auth/RequireAuth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import { selectedUser } from "../Features/loginSlice";
+import { selectedUser } from "../../Features/loginSlice";
 
 function RequireAuth() {
   const user = useSelector(selectedUser);
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -5,11 +5,11 @@ import {
 } from "react-router-dom";
 import App from "../App";
 import LoginPage from "../Pages/LoginPage";
-import RequireAuth from "../Components/RequireAuth";
 import AdminPage from "../Pages/AdminPage";
 import Welcome from "../Components/Admin/Welcome";
 import PageCreate from "../Components/Admin/Create/PageCreate";
 import AppPage from "../Pages/AppPage";
+import RequireAuth from "../Components/Auth/RequireAuth";
 
 export const router = createHashRouter(
   createRoutesFromElements(
