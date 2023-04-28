a2364fdd2d77557ebffc04d9c49b530104f8e73b
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-28 17:23:39 +0300

    margin top add

--- src/Components/Admin/Welcome.jsx
+++ src/Components/Admin/Welcome.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Welcome() {
   return (
     <div className="w-full flex justify-center ">
-      <h1 className="text-center text-3xl font-bold">Welcome To Dashboard</h1>
+      <h1 className="text-center text-3xl font-bold mt-8">
+        Welcome To Dashboard
+      </h1>
     </div>
   );
 }
