c223ebdda9a300cd4167d1dc08027cbf0725fe63
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-29 14:15:37 +0300

    header update

--- src/Components/App/Header.jsx
+++ src/Components/App/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <>
-      <header className="w-full flex items-center px-[50px] justify-between h-[85px] shadow">
+      <header className="w-full flex items-center px-[70px] justify-between h-[85px] shadow-lg">
         <a href="https://solarvis.co" target="_blank">
           <img
             className="h-[75px]"
