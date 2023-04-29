cc1fc78015471aa0faaa22e373e70d4e0779f1fe
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-29 14:46:02 +0300

    solarvis logo update

--- src/Components/App/Footer.jsx
+++ src/Components/App/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="w-full h-[30px] absolute bottom-0 flex justify-center bg-[#373242] text-white items-center">
+    <div className="w-full h-[32px] absolute bottom-0 flex justify-center bg-[#373242] text-white items-center">
       Powered by{" "}
       <a className="ml-1 underline" href="https://solarvis.co/" target="_blank">
         solarVis
