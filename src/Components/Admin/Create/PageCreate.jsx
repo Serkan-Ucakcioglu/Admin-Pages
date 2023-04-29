01165855706049d18a82b6a06f11c8d723841c90
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-29 16:47:07 +0300

    title update

--- src/Components/Admin/Create/PageCreate.jsx
+++ src/Components/Admin/Create/PageCreate.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function PageCreate() {
-  return <div>form</div>;
+  return (
+    <div className="flex justify-center mt-4">
+      <h1 className="text-center text-3xl font-bold">Create Pages</h1>
+    </div>
+  );
 }
 
 export default PageCreate;
