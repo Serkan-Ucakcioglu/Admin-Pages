d7107829a9ced1db6f690f677c4688904e8b22b8
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-04-30 12:10:28 +0300

    button disabled add

--- src/Components/Admin/Create/PageCreate.jsx
+++ src/Components/Admin/Create/PageCreate.jsx
@@ -8,18 +8,25 @@ function PageCreate() {
   const steps = {
     0: <Welcome />,
     1: "serkan",
+    2: "2 burası",
   };
 
   return (
     <div className="flex flex-col gap-y-4 items-center mt-4">
       <h1 className="text-center text-3xl font-bold">Create Pages</h1>
-      <div className="flex gap-2">
-        <h1>step1</h1>
-        <h1>step2</h1>
-        <h1>step3</h1>
+      <div className="flex items-center gap-2">
+        <h1 className={`${step == 0 && "bg-blue-500 text-white p-1 rounded"}`}>
+          step1
+        </h1>
+        <h1 className={`${step == 1 && "bg-blue-500 text-white p-1 rounded"}`}>
+          step2
+        </h1>
+        <h1 className={`${step == 2 && "bg-blue-500 text-white p-1 rounded"}`}>
+          step3
+        </h1>
       </div>
       <div>{steps[step]}</div>
-      <StepBtns setStep={setStep} />
+      <StepBtns setStep={setStep} step={step} />
     </div>
   );
 }
--- src/Components/Admin/Create/StepBtns.jsx
+++ src/Components/Admin/Create/StepBtns.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 
-function StepBtns({ setStep }) {
+function StepBtns({ setStep, step }) {
   return (
     <>
       <div className="flex gap-4 mt-8">
         <button
-          className="bg-blue-500 p-1.5 rounded hover:bg-blue-700 text-white"
+          className={`bg-blue-500 p-1 rounded hover:bg-blue-700 text-white ${
+            step == 0 ? "opacity-50" : ""
+          }`}
+          disabled={step == 0}
           onClick={() => setStep((step) => step - 1)}
         >
           Prev
         </button>
         <p>---</p>
         <button
-          className="bg-blue-500 p-1 rounded hover:bg-blue-700 text-white"
+          className={`bg-blue-500 p-1 rounded hover:bg-blue-700 text-white ${
+            step == 2 ? "opacity-50" : ""
+          }`}
+          disabled={step == 2}
           onClick={() => setStep((step) => step + 1)}
         >
           Next
