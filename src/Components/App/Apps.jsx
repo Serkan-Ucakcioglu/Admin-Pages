77ef0ce4634c9913f9e9f800699e8f9346d19eae
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-01 14:21:36 +0300

    update

--- src/Components/App/Apps.jsx
+++ src/Components/App/Apps.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Section from "./AppPage/Section";
+import { useSelector } from "react-redux";
+import { selectedFormData } from "../../Features/loginSlice";
 function App() {
+  const formData = useSelector(selectedFormData);
   return (
     <>
-      <Header />
-      <div className="flex justify-center mt-2">
-        <Section />
-      </div>
-      <Footer />
+      {!!formData.length && (
+        <>
+          {" "}
+          <Header />
+          <div className="flex justify-center mt-2">
+            <Section />
+          </div>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
