d0423cab23975ea90d73926e2f97e3a38cc2b1ae
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-05-03 10:35:33 +0300

    update

--- src/Components/App/Footer.jsx
+++ src/Components/App/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-full h-[32px] absolute bottom-0 flex justify-center bg-[#373242] text-white items-center">
-      Powered by{" "}
+      Powered by
       <a className="ml-1 underline" href="https://solarvis.co/" target="_blank">
         solarVis
       </a>
